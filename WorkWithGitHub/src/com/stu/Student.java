8fdf12eb4e2e9a2c2c2667bd838d0d0bd5149bc9
vijay raktade <vijay raktade@192.168.100.12>
2024-03-07 15:10:38 +0530

    ---by kiran

--- WorkWithGitHub/src/com/stu/Student.java
+++ WorkWithGitHub/src/com/stu/Student.java
@@ -4,5 +4,11 @@ public class Student {
    
 	public static void main(String[] args) {
 		System.out.println("Working With GitHub..");
+		
+		
+
+	}
+	public void m1() {
+		System.out.println("welcome GitHub...");
 	}
 }
